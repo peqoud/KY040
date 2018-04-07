dc8153254f3653aa28ac2aabc8facb16cddaa377
Erich Ebenhoch <erich.ebenhoch@tttech.com>
2018-04-07 18:35:38 +0200

    Changed to gpiozero for GPIO access

--- ky040/KY040.py
+++ ky040/KY040.py
@@ -1,55 +1,57 @@
 #KY040 Python Class
-#Martin O'Hanlon
-#stuffaboutcode.com
+#Erich Ebenhoch
 
-import RPi.GPIO as GPIO
-
+from gpiozero import DigitalInputDevice
 
 class KY040:
 
-    CLOCKWISE = 0
+    CLOCKWISE     = 0
     ANTICLOCKWISE = 1
 
-    def __init__(self, clockPin, dataPin, switchPin=None, rotaryCallback=None, switchCallback=None, rotaryBouncetime=250, switchBouncetime=300):
+    def __init__(self, clockPin, dataPin, switchPin=None, rotaryCallback=None, switchCallback=None, rotaryBouncetime=0.050, switchBouncetime=0.050):
         # persist values
-        self.clockPin = clockPin
-        self.dataPin = dataPin
-        self.switchPin = switchPin
-        self.rotaryCallback = rotaryCallback
-        self.switchCallback = switchCallback
+        self.clockPin       = DigitalInputDevice(clockPin, pull_up=False, bounce_time=rotaryBouncetime)
+        self.dataPin        = DigitalInputDevice(dataPin,  pull_up=False, bounce_time=rotaryBouncetime)
+        if None != switchPin:
+            self.switchPin = DigitalInputDevice(switchPin, pull_up=False, bounce_time=switchBouncetime)
+        else:
+            self.switchPin = None
+        self.rotaryCallback   = rotaryCallback
+        self.switchCallback   = switchCallback
         self.rotaryBouncetime = rotaryBouncetime
         self.switchBouncetime = switchBouncetime
 
-        #setup pins
-        GPIO.setup(clockPin, GPIO.IN)
-        GPIO.setup(dataPin, GPIO.IN)
+        # handle falling edge is a change to closed 
+        # http://henrysbench.capnfatz.com/henrys-bench/arduino-sensors-and-input/keyes-ky-040-arduino-rotary-encoder-user-manual/
+        self.clockPin.pin.edges  = "falling"
+        self.dataPin.pin.edges   = "falling"
+        self.switchPin.pin.edges = "falling"
 
-        if None != self.switchPin:
-            GPIO.setup(switchPin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
     def start(self):
-        GPIO.add_event_detect(self.clockPin, GPIO.FALLING, callback=self._clockCallback, bouncetime=self.rotaryBouncetime)
-
+        self.clockPin.pin.when_changed = self._clockCallback
+        
         if None != self.switchPin:
-            GPIO.add_event_detect(self.switchPin, GPIO.FALLING, callback=self._switchCallback, bouncetime=self.switchBouncetime)
+            self.switchPin.pin.when_changed = self._switchCallback
 
     def stop(self):
-        GPIO.remove_event_detect(self.clockPin)
+        self.clockPin.pin.when_changed = None
 
         if None != self.switchPin:
-            GPIO.remove_event_detect(self.switchPin)
+            self.switchPin.pin.when_changed = None
 
-    def _clockCallback(self, pin):
-        if GPIO.input(self.clockPin) == 0:
-            data = GPIO.input(self.dataPin)
+    def _clockCallback(self):
+        if self.clockPin.pin.state == 0:
+            data = self.dataPin.pin.state
             if data == 1:
                 self.rotaryCallback(self.ANTICLOCKWISE)
             else:
                 self.rotaryCallback(self.CLOCKWISE)
 
-    def _switchCallback(self, pin):
+    def _switchCallback(self):
         if None == self.switchPin:
             return
 
-        if GPIO.input(self.switchPin) == 0:
+        if self.switchPin.pin.state == 0:
             self.switchCallback()
+
